79566d99e9fa382c516024c79b499594f60ce5f5
Pu <pubary@proton.me>
2023-02-04 12:39:56 +0300

    added Author field for temporary use in edit form

--- NewsPaper/news/forms.py
+++ NewsPaper/news/forms.py
@@ -6,10 +6,11 @@ class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['category'].help_text = 'Выберите одну или несколько'
+        self.fields['author'].help_text = '!!! позже убрать поле !!!'
 
     class Meta:
         model = Post
-        fields = ['title', 'category', 'text',]
+        fields = ['author', 'title', 'category', 'text',]
         widgets = {
             'title': forms.Textarea(attrs={'cols': 120, 'rows': 1}),
             'text': forms.Textarea(attrs={'cols': 120, 'rows': 8}),
